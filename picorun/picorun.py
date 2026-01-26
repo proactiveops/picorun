1a16ec9a8c71374152c43509398824241fb77612
Dave Hall <skwashd@gmail.com>
2026-01-27 00:45:04 +1100

    Add copyright headers

--- picorun/picorun.py
+++ picorun/picorun.py
@@ -1,5 +1,9 @@
 """PicoRun runtime classes."""
 
+__author__ = "Dave Hall <skwashd@gmail.com>"
+__copyright__ = "Copyright 2023 - 2026, Dave Hall https://proactiveops.io"
+__license__ = "MIT"
+
 from typing import Any, ClassVar
 
 from requests import Response
